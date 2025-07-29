ea6a1aa3ab0f46d651fffd05b1c088423b574197
Bhavya <107679448+BhavyaBabariya@users.noreply.github.com>
2025-07-29 11:03:26 +0530

    initial commit

---
+++ src/Components/SearchBox/index.jsx
@@ -0,0 +1,18 @@
+import { IoSearch } from "react-icons/io5";
+import Button from "@mui/material/Button";
+
+const SearchBox = () => {
+  return (
+    <div className="d-flex align-items-center col-sm-10 part2">
+      
+      <div className="headerSearch ml-3 mr-3 ">
+        <input type="text" placeholder="Search for products..." />
+        <Button>
+          <IoSearch />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchBox;
